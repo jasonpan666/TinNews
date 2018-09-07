ba431bb7c6b2183f619710406c68851ceb616016
jasonpan666 <panzhenyujason@gmail.com>
2018-09-06 22:34:37 -0700

    second commit

---
+++ app/src/main/java/com/laioffer/tinnews/profile/ProfileModel.java
@@ -0,0 +1,4 @@
+package com.laioffer.tinnews.profile;
+
+public class ProfileModel implements ProfileContract.Model {
+}
---
+++ app/src/main/java/com/laioffer/tinnews/profile/RowTextViewModel.java
@@ -0,0 +1,4 @@
+package com.laioffer.tinnews.profile;
+
+public class RowTextViewModel {
+}
